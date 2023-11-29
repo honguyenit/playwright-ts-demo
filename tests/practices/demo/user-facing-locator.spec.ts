ed19a7c2b4592fc2631892ff4d541df419e66c71
Ho Nguyen <ho.nguyen@wizeline.com>
2023-11-29 16:49:07 +0700

    Update user-facing locator demo tc

--- tests/practices/demo/user-facing-locator.spec.ts
+++ tests/practices/demo/user-facing-locator.spec.ts
@@ -6,9 +6,16 @@ test.beforeEach(async({page}) => {
     await page.goto(url)
 })
 
-test('Get by role demo TC', async ({page}) => {
+test('User-facing locator demo TC', async ({page}) => {
     await page.getByText("Forms").click()
     await page.getByText("Form Layouts").click()
+    
     await page.getByRole("textbox",{name: "Email"}).first().fill("myemail@gmail.com")
     await page.getByRole("button", {name: "Sign in"}).first().click()
+    await page.getByLabel("Email").first().fill("new_email@gmail.com")
+    await page.getByPlaceholder("Jane Doe").fill("My Name")
+    await page.getByText("Using the Grid").click()
+    await page.getByTestId('SignIn').click()
+    await page.getByTitle("IoT Dashboard").click()
+    
 })
