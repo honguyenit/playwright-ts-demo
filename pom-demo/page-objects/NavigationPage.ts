048d0f4375f66539b4c8cd797b41c11460776415
Ho Nguyen <ho.nguyen@wizeline.com>
2023-12-08 16:08:53 +0700

    Update Navigation page object

--- pom-demo/page-objects/NavigationPage.ts
+++ pom-demo/page-objects/NavigationPage.ts
@@ -8,9 +8,41 @@ export class NavigationPage {
         this.page = page
     }
 
-    async openFormLayouts(){
-        await this.page.getByText("Forms").click()
-        await this.page.getByText("Form Layouts").click()
+    async openFormLayoutsMenu(){
+        await this.selectGroupMenuItem('Forms')
+        await this.page.getByText('Form Layouts').click()
     }
+
+    async openDatePickerMenu(){
+        await this.selectGroupMenuItem('Forms')
+        await this.page.getByText('Datepicker').click()
+    }
+
+    async openSmartTableMenu(){
+        await this.selectGroupMenuItem('Tables & Data')
+        await this.page.getByText("Smart Table").click()
+    }
+
+    async openToastrMenu(){
+        await this.selectGroupMenuItem('Modal & Overlays')
+        await this.page.getByText("Toastr").click()
+    }
+
+    async openTooltipMenu(){
+        await this.selectGroupMenuItem('Modal & Overlays')
+        await this.page.getByText("Tooltip").click()
+    }
+
+    private async selectGroupMenuItem(groupItemTitle: string){
+        const groupMenuItem = this.page.getByTitle(groupItemTitle)
+        const expandedState = await groupMenuItem.getAttribute('aria-expanded')
+        if(expandedState == 'false') {
+            await groupMenuItem.click()
+        }
+    }
+
+
+
+
 
 }
--- pom-demo/pom-tests/navigation-test.spec.ts
+++ pom-demo/pom-tests/navigation-test.spec.ts
@@ -1,14 +1,16 @@
 import {test, expect} from '@playwright/test'
 import {NavigationPage} from "../page-objects/NavigationPage"
 
-
-const url = "http://localhost:4200/pages"
-
 test.beforeEach(async({page}) => {
+    const url = "http://localhost:4200/pages"
     await page.goto(url)
 })
 
 test('Navigation bar test', async ({page})=>{
    const navPage = new NavigationPage(page)
-   navPage.openFormLayouts()
+   await navPage.openFormLayoutsMenu()
+   await navPage.openDatePickerMenu()
+   await navPage.openSmartTableMenu()
+   await navPage.openToastrMenu()
+   await navPage.openTooltipMenu()
 })
